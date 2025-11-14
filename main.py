c5608d5614c78cd603de73610c2b2634d198e792
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-11-14 17:02:24 +0200

    upload genres

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import ast
 
 url = "data/movies_metadata.csv"
 
@@ -43,10 +44,24 @@ df.dropna(inplace=True)
 
 #------------------------------------------------------
 
+def extract_genres(genre_str):
+    try:
+        genres = ast.literal_eval(genre_str)
+        return [genre['name'] for genre in genres]
+    except ValueError:
+        return []
+# print(extract_genres(df['genres'].value_counts()))
+print(df['genres'].apply(extract_genres))
+df['genres'] = df['genres'].apply(extract_genres)
+#-------------------------------------------------
+
 # print(df.head())
 # print(df.genres)
 
-genres_counts = df['genres'].value_counts()
+# genres_counts = df['genres'].value_counts()
+
+all_genres = df['genres'].explode()
+genres_counts = all_genres.value_counts()
 # print(genres_counts)
 
 # print(genres_counts.index)
